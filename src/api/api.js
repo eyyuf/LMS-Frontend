0c0309b9ccd883491f553ddcc7716367c70c7628
israel <israelashenafi29@gmail.com>
2025-12-06 07:42:24 -0800

    api key

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:4000/api',
+    baseURL: 'https://fm-bls.onrender.com/api',
     withCredentials: true, // Important for HttpOnly cookies
     headers: {
         'Content-Type': 'application/json',
